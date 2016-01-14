e3352e0d39b60a3bfb28e0760235354251cc0662
Joshua Book <joshua.book@gmail.com>
2016-01-14 11:47:56 -0500

    put countries in data object and add hover state to country button

--- app/countries.js
+++ app/countries.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var merge = require('merge');
 
 var countriesContainerStyles = {
   display: 'flex',
@@ -7,24 +8,58 @@ var countriesContainerStyles = {
   alignItems: 'center',
 }
 
-var countryStyles = {
-  padding: 5,
-  listStyle: 'none',
-  backgroundColor: 'yellow',
-  cursor: 'pointer'
-}
+var Country = React.createClass({
+  getInitialState (){
+    return {
+      hovered: false
+    }
+  },
+
+  toggleHover (){
+    this.setState({hovered: !this.state.hovered});
+  },
+
+  styles: {
+    country: {
+      padding: 5,
+      listStyle: 'none',
+      border: '1px solid black',
+      borderRadius: 4,
+      cursor: 'pointer',
+      fontFamily: "'Amatic SC', cursive",
+      fontWeight: 'bold'
+    },
+    hover: {
+      border: '1px solid white',
+      color: 'white',
+    }
+  },
+
+  render() {
+    var hoverStateStyle = merge(
+        this.state.hovered && this.styles.hover,
+        this.styles.country
+    );
+    return(
+      <li style={hoverStateStyle}
+      onMouseEnter={this.toggleHover}
+      onMouseLeave={this.toggleHover}
+      >{this.props.name}</li>
+    );
+  }
+});
+
+var COUNTRIES_DATA = [{name: 'Costa Rica'}, {name: 'Nicaragua'}];
 
 module.exports = React.createClass({
   render() {
+    var countries = COUNTRIES_DATA.map((country) => {
+      return <Country key={country.name} name={country.name}/>
+    });
     return(
       <div id='countries-drawer' style={this.props.drawerStyles}>
         <ul id='countries-container' style={countriesContainerStyles}>
-          <li style={countryStyles}>
-            Costa Rica
-          </li>
-          <li style={countryStyles}>
-            Nicaragua
-          </li>
+          {countries}
         </ul>
       </div>
     );
